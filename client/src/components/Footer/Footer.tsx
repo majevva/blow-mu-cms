7c182dbe6c60046aa4712b3a1497162f05a34b60
tmajewski <tomasz.majewski@zalando.pl>
2026-03-22 18:25:31 +0100

    feat(rebrand): switch public branding to Blow MU and remove upstream author mentions from UI

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { FaGithub } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 
 import Typography from '../Typography/Typography';
@@ -19,20 +18,14 @@ const Footer: React.FC<FooterProps> = () => {
       <footer className="m-auto mt-16 flex max-w-[1328px] flex-col-reverse items-center gap-6 rounded-t-lg bg-primary-50 px-14 py-12 dark:border-x dark:border-t dark:border-primary-900 dark:bg-primary-800/20 md:flex-row md:justify-between md:py-12">
         <div className="flex h-28 flex-col items-center text-primary-950 dark:text-primary-50 md:justify-between">
           <Typography component="h1" variant="h2">
-            Mu Online
+            Blow MU
           </Typography>
           <Typography variant="label2-r" styles="text-center">
             {t('footer.copyright1')} <br></br> {t('footer.copyright2')}
           </Typography>
           <span className="flex gap-1 font-inter text-[12px] text-primary-950 dark:text-primary-50">
             {t('footer.developedBy')}
-            <a
-              className="flex items-center gap-1 hover:text-primary-800 dark:hover:text-primary-200"
-              href="https://github.com/felipeemerson"
-              target="_blank"
-            >
-              <FaGithub className="size-3" /> felipeemerson
-            </a>
+            <strong className='font-semibold'>Blow MU Team</strong>
           </span>
         </div>
         <div className="flex place-items-center gap-6">
